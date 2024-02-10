ecd481b07c7671d5cf921c641fcdbd04f8f1ba26
zzcorczz <zzcorczz@gmail.com>
2024-02-10 01:11:47 -0800

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+
+  let appName = "Frank's App";
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to {appName}</Text>
+      <StatusBar style="auto"/>
+    </View>
+  );
+};
+
+
+/*
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
+  return (
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height:40}}
+        placeholder="Type here to translate!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      <Text style={{padding:10, fontSize: 42}}>
+        {
+          text
+            .split(' ')
+            .map(word => word && '🍕')
+            .join(' ')
+        }
+      </Text>
+    </View>
+  );
+};
+
+export default PizzaTranslator;
+*/
+
+//export default function App() {
+/*
+const Cat = props => {
+  const [isHungry, setIsHungry] = useState(true);
+
+  return (
+    <View style={styles.container}>
+      <Text>I am {props.name}, and I am {isHungry ? 'hungry':'full'}!</Text>
+      <Button
+        onPress = {() => {setIsHungry(false);}}
+        disabled={!isHungry}
+        title={isHungry ? 'Pour me some milk, please':'Thank you!'}
+      />
       <StatusBar style="auto" />
     </View>
   );
+};
+
+export default Cafe = () => {
+  return (
+    <>
+      <Cat name="MunkuStrap" />
+      <Cat name="Spot" />
+    </>
+  )
 }
+//}
+*/
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
